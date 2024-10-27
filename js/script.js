b2b2e5050dfd27f7977b17ea52f76a3a98314dc6
Buprofen <118705949+Buprofen115@users.noreply.github.com>
2024-10-27 15:26:20 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', initFullpage);
+document.addEventListener('scroll', updateHeadLink);
+window.addEventListener('beforeunload', forceRefresh);
+
+const svgObject = document.getElementById('arrow-down');
+svgObject.addEventListener('load', initSVGInteractions);
+
+function initFullpage() {
     new fullpage('#fullpage', {
         autoScrolling: true,
         scrollHorizontally: true,
     });
-});
+}
 
-window.addEventListener('beforeunload', function(event) {
-            // 强制刷新页面
-            location.reload(true);
-        });
+function initSVGInteractions() {
+    const svgDoc = svgObject.contentDocument;
+    const path = svgDoc.querySelector('path');
+
+    svgObject.addEventListener('mouseenter', () => {
+        path.setAttribute('stroke-width', '3');
+    });
+
+    svgObject.addEventListener('mouseleave', () => {
+        path.setAttribute('stroke-width', '1');
+    });
+}
+
+function forceRefresh(event) {
+    location.reload(true);
+}
+
+function updateHeadLink() {
+    const headLink = document.querySelector('.head-link');
+    const articleTitle = document.querySelector('.article-title');
+
+    if (window.scrollY > 80) {
+        headLink.textContent = articleTitle.textContent;
+    } else {
+        resetHeadLink(headLink);
+    }
+}
+
+function resetHeadLink(headLink) {
+    headLink.innerHTML = `
+        <a href="../index.html">首页</a> /
+        <span><a href="./PPT Design.html">作品集</a></span> /
+        <span><a href="./PPT Design.html">PPT 设计</a></span>
+    `;
+}
